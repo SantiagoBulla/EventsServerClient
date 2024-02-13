97eccd6390f43f0d4d663c22debbe7838cfe92e7
Santiago Bulla Perez <santiagobulla1216@gmail.com>
2024-02-12 22:11:18 -0500

    configurar conexion bd casa/trabajo

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -7,6 +7,6 @@ config();
 export default {
     host: process.env.HOST || '',
     database: process.env.DATABASE || '',
-    user: process.env.USER || '',
-    password: process.env.PASSWORD,
+    user: process.env.USER_HOME || '',
+    password: process.env.PASSWORD_HOME,
 }
